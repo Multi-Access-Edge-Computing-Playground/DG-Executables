ab6cfc317d70204b6f1b1c64231d95f8d2c313a2
Ronny Porsch <45596805+ronnyporsch@users.noreply.github.com>
2022-12-22 12:48:47 +0100

    Delete START_PYTHON_RECOGNITION.ps1

--- bin/START_PYTHON_RECOGNITION.ps1
+++
@@ -1,5 +0,0 @@
-cd C:\Users\ARAS2021
-.\DatagloveServer\Scripts\activate
-cd "C:\Users\ARAS2021\Desktop\dataGlove_ML_dynamic_gestures-main(2)\dataGlove_ML_dynamic_gestures-main\Datenhandschuh Python Server\Server Scripts\Datenhandschuh Python Server\Server Scripts"
-python "websocket_combined.py"
-pause
